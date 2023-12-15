502d0d49f23d04f812e084778f4f9912c7537485
juns <juns.g@foxmail.com>
2023-12-16 01:25:46 +0800

    feat: 添加https支持

--- evaluate.js
+++ evaluate.js
@@ -5,39 +5,42 @@
 // @description  东北大学秦皇岛分校教务处教师评价的自动化油猴脚本
 // @author       Juns
 // @match        http://jwxt.neuq.edu.cn/eams/quality/*
+// @match        https://jwxt.neuq.edu.cn/eams/quality/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // @license      MIT
 // ==/UserScript==
 
 (function () {
-    "use strict";
-    console.log("匹配成功");
-    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+  "use strict";
+  console.log("匹配成功");
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-    const baseUrl =
-      "http://jwxt.neuq.edu.cn/eams/quality/stdEvaluate!innerIndex.action";
+  const baseUrls = [
+    "http://jwxt.neuq.edu.cn/eams/quality/stdEvaluate!innerIndex.action",
+    "https://jwxt.neuq.edu.cn/eams/quality/stdEvaluate!innerIndex.action",
+  ];
 
-    // 具体评价页面执行的函数
-    async function evaluate() {
-      await delay(1000);
-      let inputs = document.querySelectorAll("input");
-      for (let i in inputs) if (inputs[i].value == 0) inputs[i].checked = true;
-      document.querySelector("textarea").value = "无";
-      await delay(500);
-      document.querySelector("#sub").click();
-    }
+  // 具体评价页面执行的函数
+  async function evaluate() {
+    await delay(1000);
+    let inputs = document.querySelectorAll("input");
+    for (let i in inputs) if (inputs[i].value == 0) inputs[i].checked = true;
+    document.querySelector("textarea").value = "无";
+    await delay(500);
+    document.querySelector("#sub").click();
+  }
 
-    // 在根页面跳转的函数
-    function jumpFn() {
-      const span = document.querySelector(".eval");
-      span.click();
-    }
+  // 在根页面跳转的函数
+  function jumpFn() {
+    const span = document.querySelector(".eval");
+    span.click();
+  }
 
-    // 初始化运行的函数
-    function init() {
-      location.href === baseUrl ? jumpFn() : evaluate();
-    }
+  // 初始化运行的函数
+  function init() {
+    baseUrls.includes(location.href) ? jumpFn() : evaluate();
+  }
 
-    init();
-  })();
+  init();
+})();
